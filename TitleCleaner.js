6fd15958900ed56596444043bcf73e93c6b957ae
WuHuan <kof1234@sina.com>
2015-05-04 11:40:15 +0800

    更改加载文件方式

--- TitleCleaner.js
+++ TitleCleaner.js
@@ -1,80 +1,56 @@
-var arr = new Array();
-var arrBL = new Array();
-var objApp = WizExplorerApp;
-var pluginPath = objApp.GetPluginPathByScriptFileName("TitleCleaner.js");
-var objWindow = objApp.Window;
+var TitleCleaner_pluginPath = objApp.GetPluginPathByScriptFileName("TitleCleaner.js");
 
-function WizAlert(msg)
+function GetHeader(src)
 {
-    objWindow.ShowMessage(msg, "Wiz", 0x00000040);
+    return objCommon.LoadTextFromFile(src);
 }
-
-function Write2Text()
-{
-	var fso = objApp.CreateActiveXObject("Scripting.FileSystemObject");
-	var f = fso.CreateTextFile("BlackList.txt", true);
-	for(var i=0;i<arr.length;i++)
-	{
-		f.write(arr[i])
-		f.WriteBlankLines(1)
-	}
-	f.Close();
-}
-
-function GetHeader(src) 
-{ 
-	var ForReading=1; 
-	var fso=objApp.CreateActiveXObject("Scripting.FileSystemObject");
-	var f = fso.OpenTextFile(src, ForReading, true); 
-	return (f.ReadAll()); 
-} 
 
 function ReadText()
 {
-	// Get black list from file...
-	arr=GetHeader(pluginPath + "BlackList.txt").split("\r\n");
-	
-	for(var i=0;i<arr.length;i++)
-	{
-		var objDoc = objApp.Window.CurrentDocument;
-		if (objDoc==null)
-        		return;
-        	else
-        	{	
-        		var arrBL=splitBlackListItem(arr[i]);
-        		
-        		if (arrBL.length==1)
-        		{
-        			var reg=new RegExp(arrBL[0],"gmi");
-        			objDoc.Title=objDoc.Title.replace(reg,"");
-        		}
-        		else
-        		{
-        			var reg=new RegExp(arrBL[0],"gmi");
-        			objDoc.Title=objDoc.Title.replace(reg,arrBL[1]);
-        			//objDoc.Title=objDoc.Title.replace(reg,"");
-        		}
-        	}
-	}
+    var arr = GetHeader(TitleCleaner_pluginPath + "BlackList.txt").split("\n");
+
+    for (var i = 0; i < arr.length; i++)
+    {
+        var objDoc = objWindow.CurrentDocument;
+        if (objDoc == null)
+        {
+            return;
+        }
+        else
+        {
+            var arrBL = splitBlackListItem(arr[i]);
+
+            if (arrBL.length == 1)
+            {
+                var reg = new RegExp(arrBL[0], "gmi");
+                objDoc.Title = objDoc.Title.replace(reg, "");
+            }
+            else
+            {
+                var reg = new RegExp(arrBL[0], "gmi");
+                objDoc.Title = objDoc.Title.replace(reg, arrBL[1]);
+            }
+        }
+    }
 }
 
 function splitBlackListItem(BLI)
 {
-	var arrBLSP = new Array();
-	arrBLSP=BLI.split('^^');
-	return arrBLSP;
+    var arrBLSP = new Array();
+    arrBLSP = BLI.split('^^');
+    return arrBLSP;
 }
 
 function InitTitleCleanerButton()
 {
-    var languangeFileName = pluginPath + "plugin.ini";
+    var languangeFileName = TitleCleaner_pluginPath + "plugin.ini";
     var buttonText = objApp.LoadStringFromFile(languangeFileName, "strBtnName");
     objWindow.AddToolButton("document", "TitleCleanerButton", buttonText, "", "OnTitleCleanerButtonClicked");
 }
 
-function OnTitleCleanerButtonClicked() 
+function OnTitleCleanerButtonClicked()
 {
-	ReadText();
+    ReadText();
 }
 
 InitTitleCleanerButton();
